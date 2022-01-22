b865cc1484de93d13fed319375929e94b0110f51
Erwan <kerbrat.erwan@gmail.com>
2022-01-22 17:58:59 +0100

    first commit

---
+++ out_react/front/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import PagePrincipale from "./components/PagePrincipale.js";
+
+class App extends React.Component {
+constructor(){
+	super()
+}
+
+render() {
+		return(
+			 <div className="App">
+				ok
+			</div>
+		)
+	}
+}
+
+export default App;
